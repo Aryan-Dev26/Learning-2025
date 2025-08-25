4e01ce9722f0d262c5f16279efbc808611c6ccb0
Aryan-Dev26 <227740686+Aryan-Dev26@users.noreply.github.com>
2025-08-25 15:00:03 +0530

    Day 1: Range Printing and Multiplication practice

---
+++ Range_Print_Multiplication.py
@@ -0,0 +1,11 @@
+for i in range(0,100):
+    print(i+1)
+
+for j in range(100,0,-1):
+    print(j)
+
+
+k = int(input("Enter a Number : "))
+m = int(input("enter a range"))
+for l in range(k,k*(m+1),k):
+    print(l)
