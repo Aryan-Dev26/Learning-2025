b854131080634a9e4533b8ef40172702c059354b
Aryan-Dev26 <227740686+Aryan-Dev26@users.noreply.github.com>
2025-08-26 12:22:33 +0530

    Day 2: List and Tuple Performed Append , sort, copy, palindrome, and count

---
+++ day3.py
@@ -0,0 +1,9 @@
+m1 = input("Enter 1st movie : ")
+m2 = input("Enter 2nd movie : ")
+m3 = input("Enter 3rd movie : ")
+movies  = []
+movies.append(m1)
+movies.append(m2)
+movies.append(m3)
+
+print(movies)
